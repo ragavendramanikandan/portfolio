0e4ac4c73ba3f0003ba088a3a211d1f283b6b9fd
ragavendramanikandan <ragavendramanikandan582gmail.com>
2026-07-22 19:07:32 +0530

    Added mobile hamburger menu

--- script.js
+++ script.js
@@ -130,3 +130,33 @@ document.querySelectorAll(".nav-links a").forEach(link => {
     });
 
 });
+
+// ======================
+// MOBILE MENU
+// ======================
+
+const menuBtn = document.querySelector(".menu-btn");
+const navLinks = document.querySelector(".nav-links");
+
+menuBtn.addEventListener("click", () => {
+
+    navLinks.classList.toggle("active");
+
+    if (navLinks.classList.contains("active")) {
+        menuBtn.innerHTML = "✕";
+    } else {
+        menuBtn.innerHTML = "☰";
+    }
+
+});
+
+document.querySelectorAll(".nav-links a").forEach(link => {
+
+    link.addEventListener("click", () => {
+
+        navLinks.classList.remove("active");
+        menuBtn.innerHTML = "☰";
+
+    });
+
+});
